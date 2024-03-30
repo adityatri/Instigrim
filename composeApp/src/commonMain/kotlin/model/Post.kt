4627b14839d7edb06b681df2a5588bba9b3c7c01
adityatri <nugroho.adityatri@gmail.com>
2024-03-30 10:29:10 +0700

    Image indicator; add buttons like, comment, share, bookmark

--- composeApp/src/commonMain/kotlin/model/Post.kt
+++ composeApp/src/commonMain/kotlin/model/Post.kt
@@ -4,9 +4,10 @@ import kotlinx.serialization.Serializable
 
 @Serializable
 data class Post(
-    val username: String,
-    val caption: String,
-    val isVerified: Boolean,
-    val likes: String,
-    val imgPath: List<String> = arrayListOf()
+    val username: String = "",
+    val caption: String = "",
+    val isVerified: Boolean = false,
+    val likes: Int = 0,
+    val imgPath: List<String> = arrayListOf(),
+    val isLiked: Boolean = false
 )
