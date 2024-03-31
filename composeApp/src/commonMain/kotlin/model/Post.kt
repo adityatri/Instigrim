7c50ba09ad0810db682cd652a6219eb8bdd81520
adityatri <nugroho.adityatri@gmail.com>
2024-03-31 13:07:35 +0700

    Add profile picture, improve LazyColumn scroll performance

--- composeApp/src/commonMain/kotlin/model/Post.kt
+++ composeApp/src/commonMain/kotlin/model/Post.kt
@@ -1,13 +1,18 @@
 package model
 
+import androidx.compose.runtime.Immutable
 import kotlinx.serialization.Serializable
+import kotlin.math.abs
 
 @Serializable
+@Immutable
 data class Post(
+    val id: Long = abs((0..999999999999).random()),
     val username: String = "",
     val caption: String = "",
     val isVerified: Boolean = false,
     val likes: Int = 0,
     val imgPath: List<String> = arrayListOf(),
-    val isLiked: Boolean = false
+    val isLiked: Boolean = false,
+    val profilePic: String = "",
 )
