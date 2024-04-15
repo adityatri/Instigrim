c3351fe7b0c5ff45d0c659baff49fa33d399da22
adityatri <nugroho.adityatri@gmail.com>
2024-04-15 13:03:48 +0700

    Remove unused files

--- composeApp/src/commonMain/kotlin/navigation/AppDestination.kt
+++
@@ -1,9 +0,0 @@
-package navigation
-
-sealed class AppDestination {
-    object Home : AppDestination()
-    object Explore : AppDestination()
-    object Create : AppDestination()
-    object Reels : AppDestination()
-    object Profile : AppDestination()
-}
